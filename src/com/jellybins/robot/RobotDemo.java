ae7948533fc9b24234f67b1674d6b22268a1af30
jellybins <da.guodong@hotmail.com>
2015-08-06 10:55:06 +0800

    添加剪切板调用，设置默认刷屏内容为‘小娜抱抱’

--- src/com/jellybins/robot/RobotDemo.java
+++ src/com/jellybins/robot/RobotDemo.java
@@ -2,6 +2,8 @@ package com.jellybins.robot;
 
 import java.awt.AWTException;
 import java.awt.Robot;
+import java.awt.Toolkit;
+import java.awt.datatransfer.StringSelection;
 import java.awt.event.KeyEvent;
 
 import com.melloware.jintellitype.HotkeyListener;
@@ -16,22 +18,41 @@ import com.melloware.jintellitype.JIntellitype;
 public class RobotDemo implements HotkeyListener {
 
 	/** 常用组合按键 */
-	public static final CombineKeys ctrlEnter = new CombineKeys(
+	@SuppressWarnings("unused")
+	private static final CombineKeys ctrlEnter = new CombineKeys(
 			KeyEvent.VK_CONTROL, KeyEvent.VK_ENTER);// Ctrl+Enter
 
-	public static final CombineKeys altS = new CombineKeys(KeyEvent.VK_ALT,
+	@SuppressWarnings("unused")
+	private static final CombineKeys altS = new CombineKeys(KeyEvent.VK_ALT,
 			KeyEvent.VK_S);// ALT+S
 
-	public static final CombineKeys ctrlV = new CombineKeys(
+	private static final CombineKeys ctrlV = new CombineKeys(
 			KeyEvent.VK_CONTROL, KeyEvent.VK_V);// Ctrl+V
 
-	/** 每keyDelay*5毫秒完成一次消息发送动作 */
-	public static final int keyDelay = 2 * 10;//按键延迟
+	/** 每 （keyDelay*5） 毫秒完成一次消息发送动作 */
+	private static final int keyDelay = 20;//按键延迟
 
-	public static boolean runFlag = false;
-
-	public RobotDemo() {
-
+	private static boolean runFlag = false;//运行标志位
+	
+	private String repeatContent = "小娜抱抱";//刷屏内容
+	
+	public static void main(String[] args) throws Exception {
+		try {
+			// 初始化
+			new RobotDemo().init();
+			Robot robot = new Robot();
+			while (true) {
+				while (runFlag) {
+					pressCombineKeys(robot, ctrlV);
+					pressKey(robot, KeyEvent.VK_ENTER);
+				}
+			}
+		} catch (AWTException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	private void init(){
 		/** 注册热键 */
 		JIntellitype.getInstance().registerHotKey(KeyEvent.VK_R,
 				JIntellitype.MOD_CONTROL + JIntellitype.MOD_ALT, KeyEvent.VK_R);
@@ -41,37 +62,19 @@ public class RobotDemo implements HotkeyListener {
 				JIntellitype.MOD_CONTROL + JIntellitype.MOD_ALT, KeyEvent.VK_X);
 
 		JIntellitype.getInstance().addHotKeyListener(this);
+		
+		/** 设置刷屏内容 */
+		setSystemClipboard(this.repeatContent);
 	}
 
-	public static void main(String[] args) throws Exception {
-
-		try {
-
-			RobotDemo rbDemo = new RobotDemo();
-
-			Robot robot = new Robot();
-
-			while (true) {
-
-				while (runFlag) {
-					rbDemo.pressCombineKeys(robot, ctrlV);
-					rbDemo.pressKey(robot,KeyEvent.VK_ENTER);
-				}
-
-			}
-		} catch (AWTException e) {
-			e.printStackTrace();
-		}
-	}
-
-	private void pressKey(Robot robot, int keyvalue) {
+	private static void pressKey(Robot robot, int keyvalue) {
 		robot.delay(keyDelay);
 		robot.keyPress(keyvalue);
 		robot.delay(keyDelay);
 		robot.keyRelease(keyvalue);
 	}
 
-	private void pressCombineKeys(Robot robot, CombineKeys combineKeys) {
+	private static void pressCombineKeys(Robot robot, CombineKeys combineKeys) {
 		robot.delay(keyDelay);
 		robot.keyPress(combineKeys.getKey1());
 		robot.delay(keyDelay);
@@ -97,11 +100,18 @@ public class RobotDemo implements HotkeyListener {
 		}
 	}
 
+	/** 设置剪切板内容 */
+	private static void setSystemClipboard(String refContent) {
+		StringSelection ss = new StringSelection(refContent.trim());
+		Toolkit.getDefaultToolkit().getSystemClipboard().setContents(ss, null);
+	}
+	
+	
 	private void destroy() {
 		JIntellitype.getInstance().unregisterHotKey(KeyEvent.VK_R);
 		JIntellitype.getInstance().unregisterHotKey(KeyEvent.VK_S);
 		JIntellitype.getInstance().unregisterHotKey(KeyEvent.VK_X);
 		System.exit(0);
 	}
-
+	
 }
